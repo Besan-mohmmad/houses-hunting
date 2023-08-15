5406449b53fb1febd1f6c74af3c3e2effe30a2f6
Besan-mohmmad <bysanmhmd72@gmail.com>
2023-08-15 13:58:22 +0300

    navbar

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,14 @@
+
+import React  from 'react'; 
+
+
+const Landing = ()=> {
+
+//   useEffect()
+
+    return <>
+    <p> welcome to landing page</p>
+    </>
+}
+
+export default Landing
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Layout from "./Layout";
+import Landing from './Landing'
+
+export {Layout, Landing}
