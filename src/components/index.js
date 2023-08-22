ba0b55d0854e3f0a3ad9f5381a16aaf8f81aa1a1
Besan-mohmmad <127625129+Besan-mohmmad@users.noreply.github.com>
2023-08-22 11:40:18 +0300

    Revert "card"

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 // import CardContainer from "./CardContainer";
 // import Cards from './Cards';
 // import Footer from './Footer';
-// import Card from './hero';
 import NavBar from "./NavBar";
 
 
 
-export {NavBar  }
+export {NavBar }
